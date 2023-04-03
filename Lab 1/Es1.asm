85d56d33f5325431bcd38c38a83e1995b859e237
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2023-04-03 15:56:16 +0200

    Update Es1.asm

--- Lab 1/Es1.asm
+++ Lab 1/Es1.asm
@@ -1,3 +1,11 @@
+# siano definite le seguenti variabili di tipo byte già inizializzate in memoria
+# n1: .byte 10
+# n2: .byte 0x10
+# n3: .byte '1'
+# sia inoltre defnita la variabile di tipo byte, non inizializzata, res
+# si calcoli res = n1 - n2 + n3
+     
+     
      .data
 
 n1:  .byte 10
