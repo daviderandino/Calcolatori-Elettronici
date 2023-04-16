8928ec7b183cdba518709ad7df353ba8883035ee
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2023-04-16 12:59:29 +0200

    Add files via upload

--- Lab 1/Es5.asm
+++ Lab 1/Es5.asm
@@ -3,16 +3,20 @@
 .ent main
 
 # Utilizzare syscall 5 per leggere da tastiera e salvare in registri
-# Da completare...
+# Scambiare i valori senza utilizzare altri registri
 
 main:
 
 	li $v0,5
 	syscall
-	move $t1,$v0
+	move $t0,$v0
 	li $v0,5
 	syscall
-	move $t2,$v0
+	move $t1,$v0
+	add $t0,$t0,$t1
+	sub $t1,$t0,$t1
+	sub $t0,$t0,$t1 
+	
 		
 	li $v0,10
 	syscall
