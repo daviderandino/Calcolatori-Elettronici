9d9feb968228a772c9e493c36c3e6cc37b9c1739
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2023-04-25 10:28:52 +0200

    Update Es1.asm

--- Lab 3/Es1.asm
+++ Lab 3/Es1.asm
@@ -10,14 +10,15 @@ main:
 	j while
 
 while:
-	li $v0,12
+	li $v0,12 # leggo un char
 	syscall
 	move $t0,$v0
 
-	beq $t0,'\n',fine
-	bgt $t0,'9',continua
+	beq $t0,'\n',fine # se il char è il carattere invio termino il programma
+	bgt $t0,'9',continua # se il char è un carattere non numerico richiedo all'utente di inserire un altro char
 	blt $t0,'0',continua
 	
+	# una volta passati questi controlli, siamo certi che l'utente ha inserito un numero
 	li $v0,4
 	la $a0,printOk
 	syscall
