7c6110633a9296ecd53fa924f9770e239a50de61
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2023-05-23 16:26:56 +0200

    Add files via upload

---
+++ Lab 7/Es2.asm
@@ -0,0 +1,38 @@
+.data
+.text
+.globl main
+.ent main
+
+main:
+	li $v0,5 # utente inserisce intero
+	syscall
+	move $a0,$v0 # metto l'intero inserito dall'utente in $a0
+	jal successivo # chiamo la procedura
+
+	move $a0,$v0 # $v0 contiene il valore di ritorno, sposto in $a0 per stamparlo
+	li $v0,1
+	syscall
+
+	li $v0,10
+	syscall
+	.end main
+
+successivo:
+	andi $t1, $a0, 1 # andi permette di verificare facilmente se un numero è pari o dispari
+	beqz $t1, pari
+	mul $a0,$a0,3
+	addi $a0,$a0,1
+	move $v0,$a0 # $v0 contiene il valore di ritorno
+	j fine
+
+pari:
+	srl $a0, $a0, 1 # shift a destra di un bit => divisione per due
+	move $v0,$a0 # $v0 contiene il valore di ritorno
+	
+
+fine:
+	jr $ra
+
+
+
+----------------
