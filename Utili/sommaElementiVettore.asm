6a52459a49f4d172a5d5284486ccd71546435673
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2023-06-22 09:50:37 +0200

    Update sommaElementiVettore.asm

--- Utili/sommaElementiVettore.asm
+++ Utili/sommaElementiVettore.asm
@@ -7,21 +7,18 @@ vett: .word 5, 7, 3, 4, 3
 
 main:
   la $t1, vett
-  and $t3,$0,$0
-  and $t4,$0,$0
+  and $t3,$0,$0 # index
+  and $t4,$0,$0 # temp reguster
 lab1: 
-  lw $t0,($t1)
-  add $t4,$t4,$t0
-  addi $t3,$t3,1
+  lw $t0,($t1) # load first element from vett
+  add $t4,$t4,$t0 # add to the temp register
+  addi $t3,$t3,1 # update index
   addi $t1,$t1,4
-  bne $t3,5,lab1
+  bne $t3,5,lab1 # 5 times
   
   la $t1,result
-  sw $t4,($t1)
+  sw $t4,($t1) # write result to memory
   
   li $v0,10
   syscall
 .end main  
-  
-  
-  
